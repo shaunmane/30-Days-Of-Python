566cba5853f68e8fa240b3784616a02515ee1f08
Shaun <131450123+shaunmane@users.noreply.github.com>
2026-01-12 16:35:11 +0000

    completed exercises level 3

---
+++ 01_Day_Introduction/day_1.py
@@ -0,0 +1,20 @@
+# Exercise: Level 3
+
+# 1. Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Shaun'))             # String
+print(type(3 == 3))              # Boolean
+print(type([1, 2, 3]))           # List
+print(type({'name':'Shaun'}))    # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+
+# 2. Find an Euclidian(Pythagorean) distance between (2, 3) and (10, 8)
+
+print((2 ** 2 + 3 ** 2) ** 0.5)  # = 3.605551275463989
+
+print((10 ** 2 + 8 ** 2) ** 0.5) # = 12.806248474865697
