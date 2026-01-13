4d9a2b1ead4588ac454ce43ca24a80b9e232aa64
Shaun <131450123+shaunmane@users.noreply.github.com>
2026-01-13 21:58:54 +0000

    added exercises level 2

---
+++ 02_Day_Variables_builtin_functions/day_2.py
@@ -0,0 +1,66 @@
+# Exercises: Level 2
+
+first_name = "Shaun"
+last_name = "Mane"
+country = "South Africa"
+city = "Johannesburg"
+age = 30
+is_married = False
+first_name, last_name, country, age, is_married = "Shaun", "Mane", "South Africa", 30, False
+
+# 1
+print(first_name, last_name, country, age, is_married)
+
+# 2
+print(len(first_name))
+
+# 3
+print(len(first_name))  
+print(len(last_name))
+
+# 4
+num_one = 5  
+num_two = 4
+
+# 5
+total_add = num_one + num_two
+
+# 6
+total_sub = num_one - num_two
+
+# 7
+total_mul = num_one * num_two
+
+# 8
+total_div = num_one / num_two
+
+# 9
+total_mod = num_two % num_one
+
+# 10
+total_pow = num_one ** num_two
+
+# 11
+total_flo = num_one // num_two
+
+# 12 The radius of a circle is 30 meters. 
+  # i. Area 
+area_of_circle = 3.14 * (30 ** 2)
+print("area_of_circle: ", area_of_circle)
+
+  # ii. Circumference
+circum_of_circle = 2 * 3.14 * 30 
+print("circum_of_circle: ", circum_of_circle)
+
+  # iii. Input
+radius = input()
+area_of_circles = 3.14 * (int(radius) ** 2)
+print("area_of_circles: ", area_of_circles)
+
+# 13. Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable names
+f_name = str(input())
+l_name = str(input())
+country_ = str(input())
+age_ = int(input())
+
+print("First Name: ", f_name, "Last Name: ",l_name, "Country: ", and age")
