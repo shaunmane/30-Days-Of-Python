4c50e3ae5c12c88448141de19b0716495de95d73
Shaun <131450123+shaunmane@users.noreply.github.com>
2026-01-16 20:37:43 +0000

    up to Q10

--- 03_Day_Operators/day_3.py
+++ 03_Day_Operators/day_3.py
@@ -9,13 +9,46 @@ age = int(30)
 height = float(178.33)
 
 # 3. Declare a variable that store a complex number
+c_num = 3j
+
 # 4. Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+b = float(input("Base: "))
+h = float(input("Height: "))
+area = 0.5 * b * h
+
 # 5. Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+length = float(input("Length: "))
+width = float(input("Width: "))
+area = length * width  
+perimeter = 2 * (length + width)
+print("Area: ", area)
+print("Perimeter: ", perimeter)
+
 # 6. Get radius of a circle using prompt. Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
+r = float(input("Radius: "))
+pi = 3.14
+area = pi * r ** 2  
+c = 2 * pi * r
+
 # 7. Calculate the slope, x-intercept and y-intercept of y = 2x -2
+x = input("x-intercept: ")
+y = 2*x - 2
+
 # 8. Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean distance between point (2, 2) and point (6,10)
+x1 = 2
+y1 = 2
+x2 = 6
+y2 = 10
+
+m = (y2 - y1)/(x2 - x1)
+
 # 9. Compare the slopes in tasks 8 and 9.
+euclidean_a = (2 ** 2 + 2 ** 2) ** 0.5
+euclidean_b = (6 ** 2 + 10 ** 2) ** 0.5
+
 # 10. Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is going to be 0.
+y = x**2 + 6*x + 9
+
 # 11. Find the length of 'python' and 'dragon' and make a falsy comparison statement.
 # 12. Use and operator to check if 'on' is found in both 'python' and 'dragon'
 # 13. I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
