4117641c55c4ed23504b516e5153332910f4e467
Shaun <shaunmane.sm@gmail.com>
2026-05-06 16:24:47 +0200

    begun day 14

---
+++ 14_Day_Higher_order_functions/day_14_level1.py
@@ -0,0 +1,13 @@
+# ─────────────────────────────────────────────
+## 💻 Exercises: Day 14
+# ─────────────────────────────────────────────
+
+# ─────────────────────────────────────────────
+### Exercises: Level 1
+# ─────────────────────────────────────────────
+# 1. Explain the difference between map, filter, and reduce.
+# 2. Explain the difference between higher order function, closure and decorator
+# 3. Define a call function before map, filter or reduce, see examples.
+# 4. Use for loop to print each country in the countries list.
+# 5. Use for to print each name in the names list.
+# 6. Use for to print each number in the numbers list.
