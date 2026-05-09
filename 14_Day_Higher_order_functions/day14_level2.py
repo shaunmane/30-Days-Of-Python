2b835898305ba66333c3b6513fbe2a51fe478c19
Shaun <shaunmane.sm@gmail.com>
2026-05-09 21:13:02 +0200

    added number 10

--- 14_Day_Higher_order_functions/day14_level2.py
+++ 14_Day_Higher_order_functions/day14_level2.py
@@ -45,7 +45,14 @@ get_string_list = map(get_string, numbers)
 print(f"List containing only strings: {list(get_string_list)}")
 
 # 10. Use reduce to sum all the numbers in the numbers list.
+from functools import reduce
+sum_two = lambda a, b: a + b
+sum_list = reduce(sum_two, numbers)
+print(f"Total of the sum of numbers in {numbers} is: {sum_list}")
+
 # 11. Use reduce to concatenate all the countries and to produce this sentence: Estonia, Finland, Sweden, Denmark, Norway, and Iceland are north European countries
+
+
 # 12. Declare a function called categorize_countries that returns a list of countries with some common pattern (you can find the [countries list](https://github.com/Asabeneh/30-Days-Of-Python/blob/master/data/countries.py) in this repository as countries.js(eg 'land', 'ia', 'island', 'stan')).
 # 13. Create a function returning a dictionary, where keys stand for starting letters of countries and values are the number of country names starting with that letter.
 # 14. Declare a get_first_ten_countries function - it returns a list of first ten countries from the countries.js list in the data folder.
