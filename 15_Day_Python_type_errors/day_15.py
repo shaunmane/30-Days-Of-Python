e200919650fc830bee2f0c1dd8854b980c2afdd7
Shaun <shaunmane.sm@gmail.com>
2026-05-13 20:06:17 +0200

    added import error

--- 15_Day_Python_type_errors/day_15.py
+++ 15_Day_Python_type_errors/day_15.py
@@ -3,20 +3,20 @@
 # ─────────────────────────────────────────────
 
 # --- SyntaxError --- #
-numbers = [1, 2, 4, 5, 7                    # SyntaxError: '[' was never closed
+#numbers = [1, 2, 4, 5, 7                    # SyntaxError: '[' was never closed
 
 # --- NameError --- #
-print(f"My name is: {name}")                # NameError: name 'name' is not defined
+#print(f"My name is: {name}")                # NameError: name 'name' is not defined
 
 # --- IndexError --- #
 numbers = [1, 2, 4, 5, 3, 7]
-print(numbers[7])                            # IndexError: list index out of range
+#print(numbers[7])                            # IndexError: list index out of range
 
 # --- ModuleNotFoundError --- #
-import bash                                  # ModuleNotFoundError: No module named 'bash'
+#import bash                                  # ModuleNotFoundError: No module named 'bash'
 
 # --- AttributeError --- #
-numbers.sorted()                             # AttributeError: 'list' object has no attribute 'sorted'. Did you mean: 'sort'?
+#numbers.sorted()                             # AttributeError: 'list' object has no attribute 'sorted'. Did you mean: 'sort'?
 
 # --- KeyError --- #
 country = {
@@ -31,16 +31,17 @@ country = {
         "flag": "https://restcountries.eu/data/afg.svg",
         "currency": "Afghan afghani"
     }
-print(country["capitol"])                    # KeyError: 'capitol'
+#print(country["capitol"])                    # KeyError: 'capitol'
 
 # --- TypeError --- #
-print(4 + "3")                               # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+#print(4 + "3")                               # TypeError: unsupported operand type(s) for +: 'int' and 'str'
 
 # --- ImportError --- #
-
+from random import numb                     # ImportError: cannot import name 'numb' from 'random'
+print(numb(numbers))
 
 # --- ValueError --- # 
 
 
 # --- ZeroDivisionError --- #
-print(10 / 0)                                # ZeroDivisionError: division by zero
+#print(10 / 0)                                # ZeroDivisionError: division by zero
