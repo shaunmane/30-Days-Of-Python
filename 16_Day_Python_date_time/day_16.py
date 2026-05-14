9a8da2cbd0c8c265f7a4fdfa8909633e68c363ba
Shaun <shaunmane.sm@gmail.com>
2026-05-14 23:11:37 +0200

    completed number 1

--- 16_Day_Python_date_time/day_16.py
+++ 16_Day_Python_date_time/day_16.py
@@ -2,8 +2,19 @@
 ## 💻 Exercises: Day 16
 # ─────────────────────────────────────────────
 
+from datetime import datetime
+
 # 1. Get the current day, month, year, hour, minute and timestamp from datetime module
+now = datetime.now()
+day = datetime.now().day
+month = datetime.now().month
+year = datetime.now().year
+minute = datetime.now().minute
+timestamp = datetime.now().timestamp
+
 # 2. Format the current date using this format: "%m/%d/%Y, %H:%M:%S")
+
+
 # 3. Today is 5 December, 2019. Change this time string to time.
 # 4. Calculate the time difference between now and new year.
 # 5. Calculate the time difference between 1 January 1970 and now.
