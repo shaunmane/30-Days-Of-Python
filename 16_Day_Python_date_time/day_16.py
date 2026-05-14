b15ec51d95e8495910c57727651f3f06b719be45
Shaun <shaunmane.sm@gmail.com>
2026-05-14 23:15:57 +0200

    completed number 2

--- 16_Day_Python_date_time/day_16.py
+++ 16_Day_Python_date_time/day_16.py
@@ -5,17 +5,19 @@
 from datetime import datetime
 
 # 1. Get the current day, month, year, hour, minute and timestamp from datetime module
-now = datetime.now()
-day = datetime.now().day
-month = datetime.now().month
-year = datetime.now().year
-minute = datetime.now().minute
-timestamp = datetime.now().timestamp
+now         = datetime.now()
+day         = datetime.now().day
+month       = datetime.now().month
+year        = datetime.now().year
+hour        = datetime.now()
+minute      = datetime.now().minute
+timestamp   = datetime.now().timestamp
 
 # 2. Format the current date using this format: "%m/%d/%Y, %H:%M:%S")
-
+print(f'Current date: {day}/{month}/{year}, {hour}:{minute}')
 
 # 3. Today is 5 December, 2019. Change this time string to time.
+
 # 4. Calculate the time difference between now and new year.
 # 5. Calculate the time difference between 1 January 1970 and now.
 # 6. Think, what can you use the datetime module for? Examples:
