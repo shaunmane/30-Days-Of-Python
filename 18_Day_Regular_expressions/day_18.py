18f51e3068df1a838efd49a750254bcefef29001
Shaun <131450123+shaunmane@users.noreply.github.com>
2026-05-25 18:43:41 +0000

    began day-18

---
+++ 18_Day_Regular_expressions/day_18.py
@@ -0,0 +1,9 @@
+# ─────────────────────────────────────────────
+## 💻 Exercises: Day 18
+# ─────────────────────────────────────────────
+
+### Exercises: Level 1
+
+# 1. What is the most frequent word in the following paragraph?
+
+paragraph = 'I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.'
